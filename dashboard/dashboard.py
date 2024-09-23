8db36ed31dd100a075d3d1d554fc8634be27d07b
henryanandsr <henryanandseptianradityo@gmail.com>
2024-09-23 18:44:14 +0700

    remove unnecessary import

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 sns.set_theme(style="dark")
 
 seller_df = pd.read_csv("../data/sellers_dataset.csv")
